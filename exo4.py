8e487210760e11ac3b6f874d26cc513577539326
Abderahmane BENALI <46283596+benaliabderrahmane@users.noreply.github.com>
2022-01-19 15:43:35 +0100

    ''

---
+++ exo4.py
@@ -0,0 +1,13 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+sense.clear()
+
+while True:
+	temp = round(sense.get_temperature(),2)
+	humidity = round(sense.get_humidity(),2)
+	print(' the temperature in the room is: {}'.format(temp))
+	print(' the humidity in the room is: {}'.format(humidity))
+	print('------------------------------')
+	time.sleep(1)
