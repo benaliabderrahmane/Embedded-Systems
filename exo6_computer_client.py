acb28da00be6c87cfc9dbb0bacb36547b98c77c1
Zakaria Bouzit <zakaria_elhabib.bouzit@g.enp.edu.dz>
2022-02-07 10:57:26 +0100

    Add files via upload
    exo6

---
+++ exo6_computer_client.py
@@ -0,0 +1,8 @@
+import socket               
+
+s = socket.socket()        
+host = '192.168.137.166'# ip of raspberry pi 
+port = 12345               
+s.connect((host, port))
+print(s.recv(1024))
+s.close()
